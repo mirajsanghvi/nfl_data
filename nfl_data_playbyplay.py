72bd57d21b1c6686096e8746ed492c4220cf3183
Miraj Sanghvi <mirajsanghvi@gmail.com>
2013-09-30 17:41:18 -0700

    changed readme and added requirements txt

--- nfl_data_playbyplay.py
+++ nfl_data_playbyplay.py
@@ -15,7 +15,6 @@ def parse_into_game_data():
 	for k, v in df2012.groupby('gameid'):
 		win_loss = pd.DataFrame(v[['nextscore', 'teamwin', 'off', 'def']].iloc[0]).T
 		_scoredb = pd.DataFrame(v[['offscore', 'defscore', 'off', 'def']].tail(1))
-		
 
 		if k[9:] == win_loss['off'] + '@' + win_loss['def']:
 		# if k.index(win_loss['off']) < k.index(win_loss['def']):
@@ -30,14 +29,12 @@ def parse_into_game_data():
 		if home == _scoredb['off']:
 			win_loss['home_score'] = _scoredb['offscore'].values[0]
 			win_loss['away_score'] = _scoredb['defscore'].values[0]
-			# print win_loss['home_score'], _scoredb['offscore']
 		elif home == _scoredb['def']:
 			win_loss['home_score'] = _scoredb['defscore'].values[0]
 			win_loss['away_score'] = _scoredb['offscore'].values[0]
 
 		for i, j in v.groupby('off'):
 			_first_downs = sum(j[j['down'] == 1]['down'])
-			# print i, _first_downs
 			if i == home:
 				win_loss['home_first_dwns'] = _first_downs
 			else:
@@ -51,18 +48,20 @@ def parse_into_game_data():
 				elif win_loss['nextscore'] > 3:
 					win_loss['first_score_td'] = 1
 			else:
-				win_loss['first_score'] = 0		
-			
-			if win_loss['home'] == win_loss['off']:
-				win_loss['home_team_win'] = 1
-			elif win_loss['home'] == win_loss['def']:
-				win_loss['home_team_win'] = 1
+				win_loss['first_score'] = 0					
+			# if win_loss['home'] == win_loss['off']:
+			# 	win_loss['home_team_win'] = 1
 
+		win_loss.drop(['off', 'def', 'teamwin'], axis = 1)
 		all_games.append(win_loss)
 	all_games_df = pd.concat(all_games, ignore_index=True)
 
 
-all_games_df[all_games_df['home_score'] > all_games_df['away_score']].shape[0]
+all_games_df['home_team_win'] = (all_games_df['home_score'] > all_games_df['away_score']).astype('int64')
+
+
+
+
 
 	# def check_if_win(row):
 	# 	first_score = 0
